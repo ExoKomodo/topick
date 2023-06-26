97ccdfcbb7cc80b3494eda04e18685a1e887d46c
James Orson <jamesaorson@gmail.com>
2023-06-26 16:41:34 -0700

    Add TODO for openai endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,3 +13,12 @@ app.get( "/", ( req, res ) => {
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
 } );
+
+/*
+    TODO: Proxy certain import openAI commands and work on helper library:
+    - https://platform.openai.com/docs/api-reference/requesting-organization
+    Example:
+    curl https://api.openai.com/v1/models \
+      -H "Authorization: Bearer $OPENAI_API_KEY" \
+      -H "OpenAI-Organization: <org-id>"
+*/
