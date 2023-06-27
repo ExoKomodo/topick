c9952abe8b206cb17658263726f7f0811d374fb7
James Orson <jamesaorson@gmail.com>
2023-06-27 09:38:37 -0700

    Add basic UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,43 +10,14 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
 .mascot {
   display: block;
   margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+
+import _ from "lodash";
+
 let query = "";
 
 // NOTE: Production
@@ -6,19 +10,46 @@ let query = "";
 // NOTE: Local dev
 const baseUrl = "http://localhost:8080";
 
-async function submitSearch() {  
-  const data = await fetch(`${baseUrl}/api/v1/topic?search=${query}`);
-  const json = await data.json();
-  console.log(json);
-}
+type SearchState = {
+  query: string,
+  results: string[],
+  isLoading?: boolean,
+};
+
+const state = reactive<SearchState>({
+  query: query,
+  results: [],
+});
+
+const submitSearch = _.debounce(async () => {
+    if (state.query === "") {
+      return;
+    }
+    state.isLoading = true;
+    const data = await fetch(`${baseUrl}/api/v1/topic?search=${state.query}`);
+    const json = await data.json();
+    state.results = json.items;
+    state.isLoading = false;
+  }, 250);
 </script>
 
 <template>
   <main>
     <h1>Topick</h1>
     <div>
-      <input id="search" v-model="query" placeholder="What kind of thing are you looking for?" class="search-box" />
+      <input id="search" v-model="state.query" placeholder="What kind of thing are you looking for?" class="search-box" />
       <button id="submit-search" @click="submitSearch">Search</button>
+    </div>
+
+    <div>
+      <ul v-if="!state.isLoading">
+        <li v-for="result in state.results">
+          {{ result }}
+        </li>
+      </ul>
+      <span v-if="state.isLoading">
+        Topick is querying the oracle...
+      </span>
     </div>
   </main>
 </template>
