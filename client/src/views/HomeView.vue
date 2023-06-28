d5557cca1f7d2306843e58f7aceb1c3ef296f6a7
James Orson <jamesaorson@gmail.com>
2023-06-28 13:46:17 -0700

    Display table for results

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -9,51 +9,71 @@ let query = "";
 
 type SearchState = {
   query: string,
-  results: string[],
+  results: any[],
+  resultObjectKeys: string[],
   isLoading?: boolean,
 };
 
 const state = reactive<SearchState>({
   query: query,
   results: [],
+  resultObjectKeys: [],
 });
 
-const submitSearch = Lib.buildDebounceAsync(async () => {
+const submitSearch = Lib.buildDebounceAsync<boolean>(async () => {
   if (state.query === "") {
-    return;
+    return false;
   }
   state.isLoading = true;
   try {
     const data = await fetch(`${Constants.BaseUrl}/api/v1/topic?search=${state.query}`);
     const json = await data.json();
     state.results = json.items;
+    state.resultObjectKeys = state.results.length > 0 ? Object.keys(state.results[0]) : [];
   }
   catch (err: any) {
     state.results = [
       `Failed to query API: ${err}`,
     ];
+    state.resultObjectKeys = [
+      'Error'
+    ];
   }
   finally {
     state.isLoading = false;
   }
-})
+
+  return false;
+});
 </script>
 
 <template>
   <main>
     <h1>Topick</h1>
     <div>
-      <input id="search" v-model="state.query" placeholder="What kind of thing are you looking for?" class="search-box" />
-      <button id="submit-search" @click="submitSearch">Search</button>
+      <form onsubmit="return false">
+        <input id="search" v-model="state.query" placeholder="What kind of thing are you looking for?" class="search-box" />
+        <button id="submit-search" @click="submitSearch">Search</button>
+      </form>
     </div>
 
-    <div>
-      <ul v-if="!state.isLoading">
-        <li v-for="result in state.results">
-          {{ result }}
-        </li>
-      </ul>
-      <span v-if="state.isLoading">
+    <div v-if="!state.isLoading && state.results.length > 0">
+      <table>
+        <caption>Search results</caption>
+        <thead>
+          <tr>
+            <th v-for="key in state.resultObjectKeys">{{ key }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="result in state.results">
+            <td v-for="key in state.resultObjectKeys">{{ result[key] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-if="state.isLoading">
+      <span>
         Topick is querying the oracle...
       </span>
     </div>
