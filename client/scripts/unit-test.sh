2d69f46d82bbddc8bb2cdab1fbaff7be3b286f5b
James Orson <jamesaorson@gmail.com>
2023-06-29 14:58:16 -0700

    PATCH: Add set flags
    Add set flags

--- client/scripts/unit-test.sh
+++ client/scripts/unit-test.sh
@@ -2,5 +2,7 @@
 
 echo 'Unit testing Topick client'
 
+set -euo pipefail
+
 cd $(dirname ${BASH_SOURCE[0]})/..
 npm run test:unit
--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -2,5 +2,7 @@
 
 echo 'Building Topick server'
 
+set -euo pipefail
+
 cd $(dirname ${BASH_SOURCE[0]})/..
 npm run build
