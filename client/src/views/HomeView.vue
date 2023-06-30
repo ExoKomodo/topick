16c64acc4e5bf853107d9de0480c87de7824921e
Nathan Barlow <nathanjbarlow@gmail.com>
2023-06-29 19:03:27 -0500

    Make any query with a url key format the result with an anchor tag

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -26,9 +26,51 @@ const submitSearch = Lib.buildDebounceAsync(async () => {
   }
   state.isLoading = true;
   try {
-    const data = await fetch(`${Constants.BaseUrl}/api/v1/topic?search=${state.query}`);
-    const json = await data.json();
-    state.results = json.items;
+    // const data = await fetch(`${Constants.BaseUrl}/api/v1/topic?search=${state.query}`);
+    // const json = await data.json();
+    //state.results = json.items;
+    state.results = [
+      {
+        "item": "Prego Traditional Pasta Sauce",
+        "url": "https://www.prego.com/our-sauces/traditional/"
+      },
+      {
+        "item": "Rao's Homemade Marinara Sauce",
+        "url": "https://www.raos.com/products/marinara-sauce/"
+      },
+      {
+        "item": "Barilla Traditional Pasta Sauce",
+        "url": "https://www.barilla.com/en-us/products/sauces/traditional-pasta-sauce"
+      },
+      {
+        "item": "Classico Tomato and Basil Pasta Sauce",
+        "url": "https://www.classico.com/product/00041129077607/tomato-basil"
+      },
+      {
+        "item": "Hunt's Tomato Sauce",
+        "url": "https://www.hunts.com/products/tomato-sauce"
+      },
+      {
+        "item": "Bertolli Organic Traditional Tomato & Basil Sauce",
+        "url": "https://www.bertolli.com/sauces/organic-traditional-tomato-basil-sauce/"
+      },
+      {
+        "item": "Newman's Own Marinara Sauce",
+        "url": "https://www.newmansown.com/foods/marinara-sauce/"
+      },
+      {
+        "item": "Ragu Old World Style Traditional Sauce",
+        "url": "https://www.ragu.com/our-sauces/old-world-style/ragu-old-world-style-traditional-sauce/"
+      },
+      {
+        "item": "Mezzetta Napa Valley Homemade Tomato & Sweet Basil Sauce",
+        "url": "https://www.mezzetta.com/our-products/detail/napa-valley-homemade/tomato-sweet-basil"
+      },
+      {
+        "item": "DeLallo Pomodoro Fresco Tomato Sauce",
+        "url": "https://www.delallo.com/shop/sauces/pomodoro-fresco-tomato-sauce/"
+      }
+    ];
     state.resultObjectKeys = state.results.length > 0 ? Object.keys(state.results[0]) : [];
   }
   catch (err: any) {
@@ -52,7 +94,8 @@ const submitSearch = Lib.buildDebounceAsync(async () => {
     <h1>Topick</h1>
     <div>
       <form onsubmit="return false">
-        <input id="search" v-model="state.query" placeholder="What kind of thing are you looking for?" class="search-box" />
+        <input id="search" v-model="state.query" placeholder="What kind of thing are you looking for?"
+          class="search-box" />
         <button id="submit-search" @click="submitSearch">Search</button>
       </form>
     </div>
@@ -68,7 +111,10 @@ const submitSearch = Lib.buildDebounceAsync(async () => {
         </thead>
         <tbody>
           <tr v-for="result in state.results">
-            <td v-for="key in state.resultObjectKeys">{{ result[key] }}</td>
+            <td v-for="key in state.resultObjectKeys">
+              <a v-if="key === 'url'" :href="result[key]" target="_blank">{{ result[key] }}</a>
+              <span v-else>{{ result[key] }}</span>
+            </td>
             <td><a :href="`https://duckduckgo.com/?q=${result.item}`">DuckDuckGo</a></td>
           </tr>
         </tbody>
@@ -88,7 +134,8 @@ const submitSearch = Lib.buildDebounceAsync(async () => {
   margin-right: 1rem;
 }
 
-th, td {
+th,
+td {
   border-bottom: 1px solid white;
 }
 </style>
